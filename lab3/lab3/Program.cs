33478d7af3d2a60260577392dd56a36c5d660d6d
m_a_ks_ <maksim.m00@mail.ru>
2021-03-12 18:59:44 +0300

    lab3

---
+++ lab3/lab3/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace lab3
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Coder coder = new Coder();
+            
+            coder.Decode(@"src/3.bmp", @"src/3.txt");
+            
+            coder.Encode(@"src/14.bmp");
+            coder.Decode();
+        }
+    }
+}
